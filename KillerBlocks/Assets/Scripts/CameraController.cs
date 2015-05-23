0249fa2d2be0368e37eb915c6823a3154a0f5ecc
Hendry Joe <hendryjoe.user@gmail.com>
2015-05-23 16:41:05 -0700

    Camera moves

---
+++ KillerBlocks/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()
+	{
+		Vector3 move = new Vector3 (offset.x, player.transform.position.y, offset.z);
+		transform.position = move;
+	}
+}
+
+//Vector3 temp = new Vector3(7.0f,0,0);
+//myGameObject.transform.position += temp;
