120b2e40285cb58cdd04682f2eed2c8f02d8c0f6
Joseph <madhatters@gmail.com>
2015-05-23 15:16:33 -0700

    Added level and player script

---
+++ KillerBlocks/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+     public float speed;
+     public float jumpHeight;
+
+
+     // Use this for initialization
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void FixedUpdate()
+     {
+          Vector3 movement;
+          float move = Input.GetAxis("Horizontal");
+          if(Input.GetButtonDown("Jump") == true)
+          {
+               movement = new Vector3(move, jumpHeight, 0.0f);
+          }
+          else
+          {
+               movement = new Vector3(move, 0.0f, 0.0f);
+          }
+          GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+     }
+
+
+}
