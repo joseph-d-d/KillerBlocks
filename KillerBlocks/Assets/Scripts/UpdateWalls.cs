531b37b17c6302051bca7d418fbe1d08fe4b382e
Hendry Joe <hendryjoe.user@gmail.com>
2015-05-23 20:03:02 -0700

    Improve, still need fixing, anybody can help?

--- KillerBlocks/Assets/Scripts/UpdateWalls.cs
+++ KillerBlocks/Assets/Scripts/UpdateWalls.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class UpdateWalls : MonoBehaviour {
 
 	Vector3 initialPos;
+	Vector3 maxHeight;
 	public GameObject player;
 	float distance; 
 	float currentHeight;
@@ -22,7 +23,17 @@ public class UpdateWalls : MonoBehaviour {
 		distance = player.transform.position.y - 0.5F;
 		maxDistance = updateMaxDistance (maxDistance, distance);
 
-		transform.localScale = findScale (1, maxDistance);
+		Vector3 temp = new Vector3 ();
+		temp = player.transform.position;
+
+		maxHeight = getMaxHeight (temp, maxHeight);
+		
+		transform.localScale = maxHeight + initialPos;
+
+
+		//transform.localScale.y = findScale (1, maxDistance);
+
+		/*
 		currentHeight = transform.localScale;
 
 		Vector3 temp = new Vector3 (initialPos.x, initialPos.y, initialPos.x);
@@ -30,15 +41,27 @@ public class UpdateWalls : MonoBehaviour {
 	
 		Vector3 adjust = new Vector3 (initialPos.x, temp.y, initialPos.z);
 		transform.position = adjust;
+		*/
 	}
 
+	/*
 	float findScale(float increase, float number){
 		return ((number + increase) / number);
 	}
+	*/
+
 
 	float updateMaxDistance(float maxDistance, float distance){
 		if (distance > maxDistance) {
 			maxDistance = distance;
 		}
+		return maxDistance;
+	}
+
+	Vector3 getMaxHeight(Vector3 compare, Vector3 max){
+		if (compare.y > max.y) {
+			return compare;
+		}
+		return max;
 	}
 }
