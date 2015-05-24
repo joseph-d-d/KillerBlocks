fc42b7b0b98e4994068a0f002b92340d8aa5ca81
Hendry Joe <hendryjoe.user@gmail.com>
2015-05-23 20:46:25 -0700

    Better version

--- KillerBlocks/Assets/Scripts/UpdateWalls.cs
+++ KillerBlocks/Assets/Scripts/UpdateWalls.cs
@@ -14,7 +14,6 @@ public class UpdateWalls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		initialScale = transform.localScale;
-		currentHeight = 1;
 	}
 	
 	// Update is called once per frame
@@ -24,33 +23,11 @@ public class UpdateWalls : MonoBehaviour {
 		distance = player.transform.position.y - 0.5F;
 		maxDistance = updateMaxDistance (maxDistance, distance);
 
-		Vector3 temp = new Vector3 (0f, player.transform.position.y, 0f);
-		//temp = player.transform.position;
-
-		maxHeight = getMaxHeight (temp, maxHeight);
-		temp = maxHeight;
+		maxHeight = getMaxHeight (maxDistance);
 		
 		transform.localScale = maxHeight + initialScale + 2*temp;
 
-
-		//transform.localScale.y = findScale (1, maxDistance);
-
-		/*
-		currentHeight = transform.localScale;
-
-		Vector3 temp = new Vector3 (initialPos.x, initialPos.y, initialPos.x);
-		temp = initialPos - temp;
-	
-		Vector3 adjust = new Vector3 (initialPos.x, temp.y, initialPos.z);
-		transform.position = adjust;
-		*/
 	}
-
-	/*
-	float findScale(float increase, float number){
-		return ((number + increase) / number);
-	}
-	*/
 
 
 	float updateMaxDistance(float maxDistance, float distance){
@@ -60,10 +37,8 @@ public class UpdateWalls : MonoBehaviour {
 		return maxDistance;
 	}
 
-	Vector3 getMaxHeight(Vector3 compare, Vector3 max){
-		if (compare.y > max.y) {
-			return compare;
-		}
-		return max;
+	Vector3 getMaxHeight(float f){
+		Vector3 h = new Vector3(0f, f, 0f);
+		return h;
 	}
 }
